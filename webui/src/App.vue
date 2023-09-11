30e8af95733005e11b08737cfbc24ad4643fcdae
Circum <93832807+Hicircum@users.noreply.github.com>
2023-09-11 23:36:38 +0800

    init webui

---
+++ webui/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="container">
+        <div class="top">
+            123
+        </div>
+        <div class="table">
+
+        </div>
+    </div>
+</template>
+
+<style lang="less" scoped>
+.container{
+    display: flex;
+    flex-direction: column;
+    max-width: 1300px;
+    margin: 30px auto;
+    .top{
+        box-sizing: border-box;
+        padding: 15px;
+        background-color: #a8a8a8;
+    }
+}
+</style>
