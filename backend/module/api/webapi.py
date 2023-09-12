e09d7fb0c735a87604ee516c15b2178936aa8d90
Circum <93832807+Hicircum@users.noreply.github.com>
2023-09-12 20:14:23 +0800

    update: add new api

--- backend/module/api/webapi.py
+++ backend/module/api/webapi.py
@@ -1,28 +1,20 @@
 from fastapi import FastAPI, BackgroundTasks
 from time import sleep
-from module.crawler.bots import start_bot, task_list
+from module.crawler.bots import start_bot, task_list, init_bot
 
 
 router = FastAPI()
-abc = [1,2,3,4,5,6,7,8,9,10]
-
-def aaaa():
-    while len(abc) > 0:
-        abc.pop()
-        sleep(5)
-
-@router.get("/api/test", tags=["test"])
-async def test():
-    import urllib.request
-    import json
-    url = "https://www.luogu.com.cn/problem/list?page=1&_contentOnly"
-    req = urllib.request.Request(url)
-    json_data = urllib.request.urlopen(req).read().decode('utf-8')
-    data = json.loads(json_data)
-    return data
+init_path = ""
 
 
-@router.get("/api/tags", tags=["test"])
+@router.on_event("startup")
+async def startup_event():
+    import os
+    global init_path
+    init_path = os.getcwd()
+
+
+@router.get("/api/tags", tags=["Proxy"])
 async def tags():
     import urllib.request
     import json
@@ -35,12 +27,22 @@ async def tags():
     return data
     
 
-@router.get("/api/start", tags=["test"])
+@router.get("/api/start", tags=["Bot"])
 async def t(background_tasks: BackgroundTasks):
     background_tasks.add_task(start_bot)
     return "ok"
 
 
-@router.get("/api/task", tags=["status"])
+@router.get("/api/task", tags=["Bot"])
 async def status():
     return task_list
+
+
+@router.get("/api/local", tags=["file"])
+async def test():
+    import os, json
+    if os.path.exists(init_path+"/download/index.json"):
+        with open(init_path+"/download/index.json", "r") as f:
+            return json.load(f)
+    else:
+        return []
